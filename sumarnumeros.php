78c8ed1997b561af7555b23e37de5107f76f22b3
Esme04 <136953598+Esme04@users.noreply.github.com>
2023-06-18 00:23:16 -0600

    Add files via upload

---
+++ sumarnumeros.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>UPJR</title>
+    <meta charset="utf-8">
+    <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+    <link rel="stylesheet" href="bootstrap-4.0.0-alpha.2/dist/css/bootstrap.css">	
+</head>
+<body>
+
+    <?php 
+
+        $num1=$_REQUEST["num1"];   
+        $num2=$_REQUEST["num2"];
+        $num3=$_REQUEST["num3"];
+        $suma=$num1+$num2+$num3;
+
+        echo($suma);  
+
+    ?> 
+
+<script  type="text/javascript" src="js/jquery-2.2.0.min.js"></script> 
+<script type="text/javascript" src="js/conecta.js"></script> <!--Vincular a archivo script!-->
+</body>
+</html>
